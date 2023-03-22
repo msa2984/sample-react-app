422a5185f99eda442708298971416a5957fb5494
Marisa Ashour <marisaashour@microsoft.com>
2023-03-22 11:31:23 -0400

    :hammer:

---
+++ SampleReactApp/webapi/Controllers/CocktailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    public class CocktailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
